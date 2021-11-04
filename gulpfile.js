489ee3cbfa37aea90c08b6df86d5d82791053f28
00-hidan-00 <david.taranov@gmail.com>
2021-11-04 19:22:33 +0200

    the end

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const { src, dest, watch, parallel, series } = require('gulp');
 
-const scss = require('gulp-sass');
+const scss = require('gulp-sass')(require('sass'));;
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify-es').default;
@@ -12,7 +12,8 @@ function browsersync() {
   browserSync.init({
     server: {
       baseDir: 'app/'
-    }
+    },
+    tunnel: true
   });
 }
 
@@ -91,6 +92,7 @@ function watching() {
   watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/*.html']).on('change', browserSync.reload);
 }
+
 
 exports.styles = styles;
 exports.watching = watching;
